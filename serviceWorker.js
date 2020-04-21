5a3648192c2b1705cbdf1984265c3e79213c0364
chandan <itsmechandan.cm@gmail.com>
2020-04-21 23:52:53 +0530

    service worker updated

--- serviceWorker.js
+++ serviceWorker.js
@@ -41,3 +41,15 @@ self.addEventListener('fetch', (e) => {
     })
   );
 });
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+          return Promise.all(keyList.map((key) => {
+        if(key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
